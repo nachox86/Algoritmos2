a2ecf84665f2602de90c99d73dd2dfbf02e4db5f
Moises Dahab <codermoikop@gmail.com>
2015-10-04 10:44:41 -0300

    Main: Improve checkInput and finish overall  functionality

--- main.c
+++ main.c
@@ -10,7 +10,6 @@
 /*
 @funcion main
 @descr Función principal del programa. Dado los argumentos de entrada toma los comentarios de funciones de un archivo de entrada y los vuelca en un archivo de salida en formato html. También genera un índice en html y un Log de eventos.
-@autor Ignacio
 @fecha 03/10/2015
 @version "2.0"
 @param argc cantidad de argumentos de entrada
@@ -20,14 +19,17 @@
 */
 int main(int argc, char *argv[]) {
 	int i, nargs = 0;
-    char *inputDir = NULL, *outputFile = NULL, *logfile = NULL;
+    char *inputDir, *outputFile, *logfile;
+    
+    Logger *log;
+    TDA_Doc *docu;
 
     if (argc < 7) {
     	printf("Not enough args\n"); /* TODO: Do something else with this */
         return -1; /* NOT ENOUGH ARGS */
     }
 
-    for (i = 1; i < argc; i++) {
+    for (i = 1; i < argc; i=i+2) {
         if ((strcmp(argv[i], "-h") == 0) || (strcmp(argv[i], "--help") == 0)) {
             showHelp();
             break;
@@ -48,7 +50,13 @@ int main(int argc, char *argv[]) {
 
     if (nargs == 3) {
     	/* We got enough arguments to proceed */
-    	/* TODO: Next step, process input */
+    	
+    	createLog(&log, logfile);
+    	createDoc(&docu, log);
+    	extraerDocumentation(docu, inputDir, outputFile);
+    	/* Create Index? */
+    	destroyDoc(&docu);
+    	closeLog(&log);
     }
 
     free(inputDir);
