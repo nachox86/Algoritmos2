77d95af2017df9a33c4e2efbe66125d70815cf99
Moises Dahab <codermoikop@gmail.com>
2015-10-08 11:49:27 -0300

    Add functions_tools.h

---
+++ functions_tools.h
@@ -0,0 +1,13 @@
+#ifndef FUNCTION_TOOLS_H_INCLUDED
+#define FUNCTION_TOOLS_H_INCLUDED
+
+#include "list_tda.h"
+#include "tda_documentador.h"
+
+void showHelp();
+
+int checkForKW(char* linea);
+
+int countFunctions(T_List* listed);
+
+#endif
