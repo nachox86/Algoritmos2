b98e7661b5e0f5eda464d21386cb651f2df917fd
Mike Dahab <moikop@live.com>
2015-10-11 18:27:55 -0300

    Docu: Fix file name management

--- tda_documentador.c
+++ tda_documentador.c
@@ -64,8 +64,9 @@ int extractDocumentation(TDA_Doc *docu, char *inputDir, char *outputFile) {
         while (dp = readdir(dir)) {
             if ((strcmp(dp->d_name, ".") != 0) && (strcmp(dp->d_name, "..") != 0)) {
                 buffer = realloc(buffer, sizeof(char**) * (n+2));
-                buffer[n] = malloc(strlen(dp->d_name) + 1);
-                strcpy(buffer[n], dp->d_name);
+                buffer[n] = malloc(strlen(inputDir) + strlen(dp->d_name) + 1);
+                strcpy(buffer[n], inputDir);
+                strcat(buffer[n], dp->d_name);
                 n++;
             }
         }
