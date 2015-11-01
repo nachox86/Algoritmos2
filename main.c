3d6c30b642a2bab74cf08e357fb8c29b8264b767
nachox86 <cignaciogarcia@gmail.com>
2015-11-01 16:27:38 -0300

    saco las funciones de busqueda ordenada que en el main no tienen que ir

--- main.c
+++ main.c
@@ -2,10 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include "tda_documentador.h"
-#include "tda_nodo.h"
 #include "htmlParser.h"
 #include "logger_tda.h"
 
+#define INDEX_PREFFIX "index preffix"        /*escribir q va en estas ctes*/
+#define INDEX_PREFFIX_NO_EXT "no ext"
+#define RES_WRONG_ARGS -6
 #define ARGS_HELP1 "-h"
 #define ARGS_HELP2 "--help"
 #define ARGS_INPUT_FILE "-i"
@@ -23,78 +25,79 @@
 @fecha 03/10/2015
 @version "1.0"
 @param ninguno
-@pre ninguna condiciÃ³n necesaria
+@pre ninguna condición necesaria
 @pos devuelve en pantalla la ayuda del programa.
 */
 void showHelp()
 {
-    printf("Este programa permite obtener en formato html, la documentaciÃ³n provista de un archivo de texto.\n");
+    printf("Este programa permite obtener en formato html, la documentación provista de un archivo de texto.\n");
     printf("Uso:\n");
     printf("Documentador.exe -i archivo_a_documentar.c -l archivo_log.txt -o archivo_documentado.html\n");
     printf("Documentador.exe -h\n");
     printf("Documentador.exe --help\n");
-    printf("Descripcion de los parÃ¡metros:\n");
-    printf("-i: indica el nombre del archivo de entrada del que se toma la documentaciÃ³n.\n");
+    printf("Descripcion de los parámetros:\n");
+    printf("-i: indica el nombre del archivo de entrada del que se toma la documentación.\n");
     printf("-l: indica el nombre del archivo de loggeo de eventos.\n");
-    printf("-o: indica el nombre del archivo de salida de la documentaciÃ³n en formato html.\n");
+    printf("-o: indica el nombre del archivo de salida de la documentación en formato html.\n");
     printf("-h o --help: muestra esta ayuda.\n");
+}
+
+
+int search_site(straight_list_t *lp, const void* data, straight_list_movement_t *mov)
+{
+	void *current_data; /*referencia de donde se va a guardar el dato/elemento del corriente de la lista*/
+	char **cdata, **rdata;
+
+	if(straight_list_is_empty(lp))
+		return FALSE;
+
+	straight_list_get(lp,current_data);
+
+	cdata = ((char**)current_data);
+	rdata = ((char**)data);
+	/*validar que pudo hacer el get porque usa la función de copy*/
+    /*data: sería un char**, con data[0]="nombre de la funcion", data[1]="nombre del archivo"*/
+
+   	if (strcmp( cdata[0], rdata[0]) > 0)
+	{
+		straight_list_move(&lp,straight_list_first);
+		straight_list_get(lp,current_data);
+	}
+    while(strcmp(data,cdata[0])>0 && straight_list_move(&lp,straight_list_next))
+	{
+		straight_list_get(lp,current_data);
+	}
+	if(strcmp(rdata[0],cdata[0])<0)
+	{
+		*mov = straight_list_first;
+	}
+	else
+		*mov = straight_list_next;
+
+	return TRUE;
 }
 
 
 /*
 @funcion straight_list_order_insert
-@descr Esta funciÃ³n realiza la inserciÃ³n ordenada de un dato, en la posiciÃ³n correcta en una lista. Realiza la comparaciÃ³n entre nombres de funciones para ordernar.
+@descr Esta función realiza la inserción ordenada de un dato, en la posición correcta en una lista. Realiza la comparación entre nombres de funciones para ordernar.
 @autor Ignacio
 @fecha 14/10/2015
 @version "1.0"
 @param lp referencia a la lista
 @param data referencia al dato a guardar en la lista
 @pre la lista debe estar creada
-@pos se guardarÃ¡ en la posiciÃ³n que deba de acuerda al criterio de ordenamiento.
+@pos se guardará en la posición que deba de acuerda al criterio de ordenamiento.
 */
-int iListOrderInsert(T_List *lp, void* data)
+int iListOrderInsert(straight_list_t *lp, void* data)
 {
     int insert;
-    T_Move mov = straight_list_first;
+    straight_list_movement_t mov = straight_list_first;
 	search_site(lp,data,&mov);
-	insert = InsertE(&lp,mov,data);
+	insert = straight_list_insert(&lp,mov,data);
 	return insert;
 }
 
-
-int search_site(T_List *lp, const void* data, T_Move *mov)
-{
-	void *current_data; /*referencia de donde se va a guardar el dato/elemento del corriente de la lista*/
-	char **cdata, **rdata;
-
-	if(EmptyList(lp))
-		return FALSE;
-
-	GetC(lp,current_data);
-
-	cdata = ((char**)current_data);
-	rdata = ((char**)data);
-	/*validar que pudo hacer el get porque usa la funciÃ³n de copy*/
-    /*data: serÃ­a un char**, con data[0]="nombre de la funcion", data[1]="nombre del archivo"*/
-
-   	if (strcmp( cdata[0], rdata[0]) > 0)
-	{
-		MoveC(&lp,M_First);
-		GetC(lp,current_data);
-	}
-    while(strcmp(data,cdata[0])>0 && MoveC(&lp,M_Next))
-	{
-		GetC(lp,current_data);
-	}
-	if(strcmp(rdata[0],cdata[0])<0)
-	{
-		*mov = M_Prev;
-	}
-	else
-		*mov = M_Next;
-
-	return TRUE;
-}
 
 
 /*
@@ -105,38 +108,37 @@ int search_site(T_List *lp, const void* data, T_Move *mov)
 @version "1.0"
 @param listed referencia a la lista
 @pre la lista debe estar creada
-@pos DevolverÃ¡ la cantidad de nodos que encontrÃ³ en la lista.
+@pos Devolverá la cantidad de nodos que encontró en la lista.
 */
-int countFunctions(T_List* listed)
+int countFunctions(straight_list_t* listed)
 {
     int i = 0;
 
-    if(EmptyList(listed)!=FALSE)
+    if(straight_list_is_empty(listed)!=FALSE)
     {
-        MoveC(&listed,M_First);
+        straight_list_move(&listed,straight_list_first);
         do{
             i++;
-        }while(MoveC(&listed,M_Next)!=FALSE);
+        }while(straight_list_move(&listed,straight_list_first)!=FALSE);
     }
-    MoveC(&listed,M_First);
+    straight_list_move(&listed,straight_list_first);
     return i;
 }
 
 /*
 @funcion main
-@descr FunciÃ³n principal del programa. Dado los argumentos de entrada toma los comentarios de funciones de un archivo de entrada y los vuelca en un archivo de salida en formato html. TambiÃ©n genera un Ã­ndice en html y un Log de eventos.
+@descr Función principal del programa. Dado los argumentos de entrada toma los comentarios de funciones de un archivo de entrada y los vuelca en un archivo de salida en formato html. También genera un índice en html y un Log de eventos.
 @fecha 03/10/2015
 @version "2.0"
 @param argc cantidad de argumentos de entrada
 @param argv[] array de argumentos de entrada
-@pre Se debe ejecutar el modo de ayuda antes de usarlo para saber quÃ© argumentos pasarle: tp2.exe -h o tp2.exe --help
-@pos DevolverÃ¡ en un archivo de salida, el Ã­ndice con todas las funciones documentadas, en otro la documentaciÃ³n de esas funciones.
+@pre Se debe ejecutar el modo de ayuda antes de usarlo para saber qué argumentos pasarle: tp2.exe -h o tp2.exe --help
+@pos Devolverá en un archivo de salida, el índice con todas las funciones documentadas, en otro la documentación de esas funciones.
 */
 int main(int argc, char *argv[]) {
 	int i, nargs = 0;
-    int inputIndex, outputIndex, logIndex;
+    char *inputDir, *outputFile, *logFile, *indexFile;
     char* token;
-    char *indexFile;
 
     Logger *log;
     TDA_Doc *docu;
@@ -150,15 +152,21 @@ int main(int argc, char *argv[]) {
     for (i = 1; i < argc; i=i+2) {
         if ((strcmp(argv[i], ARGS_HELP1) == 0) || (strcmp(argv[i], ARGS_HELP2) == 0)) {
             showHelp();
+            free (inputDir);
+            free (logFile);
+            free (outputFile);
             return RES_OK;
         } else if (strcmp(argv[i], ARGS_INPUT_FILE) == 0) {
-            inputIndex = i;
+            inputDir = malloc(sizeof(char) * strlen(argv[i+1]) + 1);
+            strcpy(inputDir, argv[i+1]);
             nargs++;
         } else if (strcmp(argv[i], ARGS_LOG_FILE) == 0) {
-            logIndex = i;
+            logFile = malloc(sizeof(char) * strlen(argv[i+1]) + 1);
+            strcpy(logFile, argv[i+1]);
             nargs++;
         } else if (strcmp(argv[i], ARGS_OUTPUT_FILE) == 0) {
-            outputIndex = i;
+            outputFile = malloc(sizeof(char) * strlen(argv[i+1]) + 1);
+            strcpy(outputFile, argv[i+1]);
             nargs++;
         }
     }
@@ -166,12 +174,12 @@ int main(int argc, char *argv[]) {
     if (nargs == ARGS_REQ) {
     	/* We got enough arguments to proceed */
 
-    	logLightInit(log, argv[logIndex], fatal);
+    	logLightInit(log, logFile, fatal);
     	createDoc(&docu, log);
-    	extractDocumentation(docu, argv[inputIndex], argv[outputIndex]);
+    	extractDocumentation(docu, inputDir, outputFile);
 
     	/*armo el nombre del archivo de indice*/
-        token = strtok(argv[outputIndex],HTML_EXT);
+        token = strtok(outputFile,HTML_EXT);
 
         if(!token) {
             indexFile = malloc(strlen(INDEX_PREFFIX_NO_EXT) + strlen(token));
@@ -187,7 +195,15 @@ int main(int argc, char *argv[]) {
     else
     {
         showHelp();
+        free(inputDir);
+    	free(outputFile);
+    	free(logFile);
         return RES_WRONG_ARGS;
     }
+
+    free(inputDir);
+    free(outputFile);
+    free(logFile);
+
     return RES_OK;
 }
