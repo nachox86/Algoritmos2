22b49cba7d2a7c2c1aa87e14e2193f6a23b2c216
Mike Dahab <moikop@live.com>
2015-11-29 15:22:43 -0300

    Add test input files

---
+++ input/htmlParser.h
@@ -0,0 +1,63 @@
+#ifndef HTMLPARSER_H_INCLUDED
+#define HTMLPARSER_H_INCLUDED
+
+#include <stdio.h>
+#include "tda_documentador.h"
+
+#define SUCCESS     1
+#define ERROR       0
+
+#define KW_TITLE       "@titulo"
+#define KW_SUBTITLE    "@subtitulo"
+#define KW_FUNCTION    "@funcion"
+#define KW_DESCRIPTION "@descr"
+#define KW_AUTHOR      "@autor"
+#define KW_DATE        "@fecha"
+#define KW_VERSION     "@version"
+#define KW_PARAM       "@param"
+#define KW_RETURN      "@return"
+#define KW_PRE         "@pre"
+#define KW_POST         "@pos"
+
+typedef struct {
+    int init;
+    char *path;
+    FILE *file;
+} htmlFile;
+
+/*
+	@funcion createHtmlFile
+	@descr Inicializa y crea el archivo html donde guardara la documentacion
+	@param output htmlFile a inicializar
+    @param path direccion del archivo donde guardar la documentacion
+	@author mike
+	@return SUCCESS o ERROR, dependiendo del caso
+*/
+
+int createHtmlFile(htmlFile *output, char *path);
+
+/*
+    @funcion parseStringToHtml
+    @descr Convierte el string del comentario a formato HTML y lo guarda en el archivo previamente creado
+    @author mike
+    @param output htmlFile donde guardar el comentario
+    @param line comentario a procesar y guardar en el archivo HTML
+    @pre output debe haber sido creado en inicializado previamente.
+    @pre line debe ser el comentario con el formato establecido en el enunciado del TP. i.e: "@KeyWord arg1 [arg2] ...".
+    @return En caso de error devuelve ERROR (0), SUCCESS (1) en caso contrario
+*/
+
+int parseStringToHtml(htmlFile *output, t_keyword comment);
+
+/*
+    @funcion closeHtmlFile
+    @descr Cierra y finaliza la escritura del archivo HTML donde se guardaron los comentarios
+    @param output htmlFile a cerrar
+    @pre output debe haber sido previamente creado
+    @return En caso de error devuelve ERROR (0), SUCCESS (1) en caso contrario
+*/
+
+int closeHtmlFile(htmlFile *output);
+
+
+#endif
---
+++ input/tda_documentador.h
@@ -0,0 +1,79 @@
+#ifndef DOCU_TDA_H_INCLUDED
+#define DOCU_TDA_H_INCLUDED
+
+#include "logger_tda.h"
+#include "straight_list.h"
+
+#define RES_OK      0
+#define RES_ERROR   1
+
+typedef struct {
+    char* tag;
+    char* name;
+    char* value;
+} t_keyword;
+
+typedef struct {
+    char *inputFile;
+    char *outputFile;
+    Logger *logFile;
+    straight_list_t* listado;
+    straight_list_t* indice;
+} TDA_Doc;
+
+    /*
+    @funcion createDoc
+    @descr Esta es una primitiva del TDA_Doc que crea el documentador.
+    @autor Ignacio
+    @fecha 02/10/2015
+    @version "1.0"
+    @param *docu documentador a ser creado
+    @param Log objeto que permite el logueo
+    @pre ninguna condición necesaria
+    @pos devolverá el código de éxito si pudo crear y solicitar los recursos necesarios para el documentador, en caso contrario devolverá el código de error.
+    */
+    int createDoc(TDA_Doc **docu, Logger *log);
+
+    /*
+    @funcion extractDocumentation
+    @descr Esta es una primitiva del TDA_Doc que toma los comentarios del archivo de texto de entrada y los vuelca en formato html en el archivo de salida.
+    @autor Ignacio
+    @fecha 02/10/2015
+    @version "1.0"
+    @param *docu es el documentador.
+    @param *inputFile es el archivo de entrada del que se toman los comentarios.
+    @param *outputFile es el archivo de salida donde estará toda la información en formato html.
+    @pre el documentador debe estar creado e inicializado
+    @pos Devuelve RES_OK si pudo extraer la información a documentar del archivo_entrada, escribiéndola en el archivo_salida y lo registra en el log.
+         Devuelve RES_ERROR en otro caso, o la constante que se defina pertinentemente y lo registra en el log en modo ERROR.
+    */
+    int extractDocumentation(TDA_Doc *docu, char *inputFile, char *outputFile);
+
+    /*
+    @funcion createIndex
+    @descr Esta es una primitiva del TDA_Doc que toma la información guardada en la estructura del documentador, y vuelca en una archivo en formato html un índice de las funciones existentes del que fue el archivo de entrada.
+    @autor Ignacio
+    @fecha 02/10/2015
+    @version "1.0"
+    @param *docu es el documentador.
+    @param *indexFile es el archivo de índices.
+    @pre Documentador creado e inicializado.
+    @pos El archivo de salida “arch_indice” contiene el índice construido en formato html.
+         Devuelve RES_OK en caso de que todo haya funcionado correctamente.
+         Decuelve RES_ERROR en otro caso.
+    */
+    int createIndex(TDA_Doc *docu, char *indexFile);
+
+    /*
+    @funcion destroyDoc
+    @descr Esta es una primitiva del TDA_Doc que destruye el documentador credo anteriormente, devolviendo los recursos utilizados al sistema.
+    @autor Ignacio
+    @fecha 02/10/2015
+    @version "1.0"
+    @param *docu es el documentador.
+    @pre Documentador creado e inicializado.
+    @pos Devuelve RES_ERROR en otro caso, o la constante que se defina pertinentemente y lo registra en el log en modo ERROR.
+         En ambos casos registra la actividad en el log según corresponda
+    */
+    int destroyDoc(TDA_Doc **docu);
+#endif
